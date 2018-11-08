1f1fb4a3b7c873e06f1d3f91ff3df1a7bd835b93
Hermit <mathias0512@gmail.com>
2018-11-08 20:47:13 +0100

    First take on making it oop

---
+++ RoleAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleProj
+{
+    class RoleAttribute
+    {
+        public int HealtPoints { get; set; }
+        public int Armor { get; set; }
+        public short Range { get; set; }
+        public int GroundDamage { get; set; }
+        public int AirDamage { get; set; }
+        public int EnergyPoints { get; set; }
+        public short MovmentSpeed { get; set; }
+        public int SightRange { get; set; }
+    }
+}
