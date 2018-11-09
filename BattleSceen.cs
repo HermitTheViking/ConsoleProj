2ca3e71f2a03ce3b17b23fb7d9d174c8a77b4da4
Hermit <mathias0512@gmail.com>
2018-11-09 09:26:34 +0100

    Trying to make Fight.cs more oop in BattleSceen.cs
    Trying to make Role.cs more oop in RoleDescription.cs, RoleAttribute.cs, Terran.cs, Zerg,cs

---
+++ BattleSceen.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleProj
+{
+    class BattleSceen
+    {
+        private int DamageDelt (int attackDamage, int defenderArmor)
+        {
+            int damgeDelt = 0;
+            return damgeDelt;
+        }
+
+        private int DamageHealed(int medicEnergy, int patientHealtPoint)
+        {
+            int healtPointHealed = 0;
+            return healtPointHealed;
+        }
+
+        private int EnergyDraw(int energy)
+        {
+            int energyLeft = 0;
+            return energyLeft;
+        }
+
+        private int EnergyRecovery()
+        {
+            return 1;
+        }
+
+        private int RNG(int min, int max)
+        {
+            Random rnd_Random = new Random();
+            int rnd_Int = rnd_Random.Next(min, max);
+
+            return rnd_Int;
+        }
+    }
+}
---
+++ RoleDescription.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleProj
+{
+    class RoleDescription
+    {
+        public string HealtPointLine {get; set; }
+        public string ArmorLine { get; set; }
+        public string RangeLine { get; set; }
+        public string GroundDamageLine { get; set; }
+        public string AirDamageLine { get; set; }
+        public string EnergyPointsLine { get; set; }
+        public string MovmentSpeedLine { get; set; }
+        public string SightRangeLine { get; set; }
+    }
+}
