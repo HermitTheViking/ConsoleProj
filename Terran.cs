77c763d3ea32d9727bd553c2dae57ca7675782a8
Hermit <mathias0512@gmail.com>
2018-11-09 08:45:42 +0100

    Made 2 constructors in Zerg.cs and Terran.cs

--- Terran.cs
+++ Terran.cs
@@ -4,8 +4,18 @@ using System.Text;
 
 namespace ConsoleProj
 {
-    class Terran
+    class Terran : RoleAttribute
     {
-
+        public Terran(int hp, int armor, short range, int ground, int air, int energy, short speed, int shigt)
+        {
+            HealtPoints = hp;
+            Armor = armor;
+            Range = range;
+            GroundDamage = ground;
+            AirDamage = air;
+            EnergyPoints = energy;
+            MovmentSpeed = speed;
+            SightRange = shigt;
+        }
     }
 }
--- Zerg.cs
+++ Zerg.cs
@@ -4,8 +4,18 @@ using System.Text;
 
 namespace ConsoleProj
 {
-    class Zerg
+    class Zerg : RoleAttribute
     {
-
+        public Zerg(int hp, int armor, short range, int ground, int air, int energy, short speed, int shigt)
+        {
+            HealtPoints = hp;
+            Armor = armor;
+            Range = range;
+            GroundDamage = ground;
+            AirDamage = air;
+            EnergyPoints = energy;
+            MovmentSpeed = speed;
+            SightRange = shigt;
+        }
     }
 }
